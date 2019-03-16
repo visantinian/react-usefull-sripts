cc1ab956e48c7eb30ea3c837c117ccaf97a1864f
visantinian <36194899+visantinian@users.noreply.github.com>
2019-03-16 16:54:54 +0200

    added initial foldername configs

--- generate-components-files.js
+++ generate-components-files.js
@@ -12,23 +12,32 @@ const folderNames = [
     'random-planet',
     'starship-details'
 ];
+const capitalize = (s) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
 folderNames.forEach(function(folderName) {
     mkdirp(`./src/components/${folderName}`, function (err){
         if (err) console.error(err);
         else console.log(`${folderName} created wapeow!`)
     });
 
-    fs.writeFile(`./src/components/${folderName}/${folderName}.js`, 'js file', function (err) {
+    fs.writeFile(`./src/components/${folderName}/${folderName}.js`,
+      `import React from 'react';\n\nimport './${folderName}.css'
+      \nconst ${capitalize(folderName)} = () => {\n}`; ', function (err) {
         if (err) throw err();
         console.log(`File ${folderName}.js is created successfully.`);
     });
 
-    fs.writeFile(`./src/components/${folderName}/${folderName}.css`, `css file`, function(err) {
+    fs.writeFile(`./src/components/${folderName}/${folderName}.css`, '', function(err) {
         if (err) throw err();
         console.log(`File ${folderName}.css is created successfully.`);
     });
 
-    fs.writeFile(`./src/components/${folderName}/index.js`, `index.js`, function(err) {
+    fs.writeFile(`./src/components/${folderName}/index.js`,
+       `import ${capitalize(folderName)} from './${folderName}';
+        \nexport default ${capitalize(folderName);}  `, function(err) {
         if (err) throw err();
         console.log(`File index.js  in ${folderName}is created successfully.`);
     })
