4ea3e254b2520588ca1d96ca97bb9104b8a63e26
visantinian <36194899+visantinian@users.noreply.github.com>
2019-03-16 18:41:46 +0200

    Fixed some errors and functionality

--- generate-components-files.js
+++ generate-components-files.js
@@ -1,5 +1,3 @@
-
-
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 
@@ -10,35 +8,48 @@ const folderNames = [
     'person-details',
     'planet-details',
     'random-planet',
-    'starship-details'
+    'starship-details',
+    'stupid-ass-long-kebab-test-class'
 ];
 const capitalize = (s) => {
-  if (typeof s !== 'string') return ''
-  return s.charAt(0).toUpperCase() + s.slice(1)
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
+const removeKebab = (a) => {
+    if (typeof a !== 'string') {
+        return ''
+    }
+    const target = [];
+    const splitString = a.split("-");
+    const length = splitString.length;
+    for (var j = 0; j <= length; j++) {
+        target.push(capitalize(splitString[j]));
+    }
+    return target.join('');
 }
 
-folderNames.forEach(function(folderName) {
-    mkdirp(`./src/components/${folderName}`, function (err){
+folderNames.forEach(function (folderName) {
+    mkdirp(`./src/components/${folderName}`, function (err) {
         if (err) console.error(err);
         else console.log(`${folderName} created wapeow!`)
     });
 
     fs.writeFile(`./src/components/${folderName}/${folderName}.js`,
-      `import React from 'react';\n\nimport './${folderName}.css'
-      \nconst ${capitalize(folderName)} = () => {\n}`; ', function (err) {
-        if (err) throw err();
-        console.log(`File ${folderName}.js is created successfully.`);
-    });
+        `import React from 'react';\n\nimport './${folderName}.css'
+      \nconst ${removeKebab(folderName)} = () => {\n};`, function (err) {
+            if (err) console.error(err);
+            console.log(`File ${folderName}.js is created successfully.`);
+        });
 
-    fs.writeFile(`./src/components/${folderName}/${folderName}.css`, '', function(err) {
-        if (err) throw err();
+    fs.writeFile(`./src/components/${folderName}/${folderName}.css`, '', function (err) {
+        if (err) console.error(err);
         console.log(`File ${folderName}.css is created successfully.`);
     });
 
     fs.writeFile(`./src/components/${folderName}/index.js`,
-       `import ${capitalize(folderName)} from './${folderName}';
-        \nexport default ${capitalize(folderName);}  `, function(err) {
-        if (err) throw err();
-        console.log(`File index.js  in ${folderName}is created successfully.`);
-    })
+        `import ${removeKebab(folderName)} from './${folderName}';
+        \nexport default class ${removeKebab(folderName)};`, function (err) {
+            if (err) console.error(err);
+            console.log(`File index.js  in ${folderName}is created successfully.`);
+        })
 });
